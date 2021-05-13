1898e3de4b8deee2a3ded242fac32906582d1676
dream-dfe <78368363+dream-dfe@users.noreply.github.com>
2021-05-13 16:22:00 +0200

    Update mail-script.js

--- js/mail-script.js
+++ js/mail-script.js
@@ -1,7 +1,7 @@
     // -------   Mail Send ajax
 
      $(document).ready(function() {
-        var form = $('#myForm'); // contact form
+        var form = $('#'); // contact form
         var submit = $('.submit-btn'); // submit button
         var alert = $('.alert-msg'); // alert div for show alert message
 
@@ -10,7 +10,7 @@
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
-                url: 'mail.php', // form action url
+                url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScavfO0uhXYj-_gnisQrrFKJ_vw8oi8yTaVVVr9J2AuVPBc7Q/formResponse', // form action url
                 type: 'POST', // form submit method get/post
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
@@ -21,11 +21,19 @@
                 success: function(data) {
                     alert.html(data).fadeIn(); // fade in response data
                     form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
+                    submit.attr("style", "display: none !important"); // reset submit button text
+
                 },
                 error: function(e) {
                     console.log(e)
                 }
             });
+
         });
+
+        
     });
+
+
+
+    
